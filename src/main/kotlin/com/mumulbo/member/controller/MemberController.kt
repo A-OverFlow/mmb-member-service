ef454a6233852e133357b9bbfb399decfb466852
joonhee.song <mike.urssu@gmail.com>
2025-04-12 18:28:50 +0900

    Feat: 인증 관련 로직 제거
    - Spring Security 제거
        - `BCryptPasswordEncoder`를 사용하기 위해 `spring-security-crypto` 추가
    - Redis 제거
    - 인증 관련 로직 제거
    - Commit 메시지, PR 템플릿 추가
    
    Resolves: #28
    Related to: #27

--- src/main/kotlin/com/mumulbo/member/controller/MemberController.kt
+++ src/main/kotlin/com/mumulbo/member/controller/MemberController.kt
@@ -1,15 +1,8 @@
 package com.mumulbo.member.controller
 
-import com.mumulbo.auth.exception.InvalidTokenException
-import com.mumulbo.auth.service.JwtTokenProvider
-import com.mumulbo.common.exception.UnauthorizedException
-import com.mumulbo.member.dto.response.MemberGetResponse
 import com.mumulbo.member.service.MemberService
-import jakarta.servlet.http.HttpServletRequest
 import org.springframework.http.HttpStatus
-import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.DeleteMapping
-import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.ResponseStatus
@@ -18,26 +11,8 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 @RequestMapping("/api/v1/members")
 class MemberController(
-    private val memberService: MemberService,
-    private val jwtTokenProvider: JwtTokenProvider
+    private val memberService: MemberService
 ) {
-    @GetMapping("/me")
-    fun getMember(request: HttpServletRequest): ResponseEntity<MemberGetResponse> {
-        val header = request.getHeader("Authorization")
-        if (header == null || !header.startsWith("Bearer ")) {
-            throw UnauthorizedException()
-        }
-
-        val accessToken = header.substring(7)
-        if (!jwtTokenProvider.isValid(accessToken)) {
-            throw InvalidTokenException()
-        }
-
-        val id = jwtTokenProvider.getIdFromToken(accessToken)
-        val response = memberService.getMember(id)
-        return ResponseEntity.status(HttpStatus.OK).body(response)
-    }
-
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     fun withdraw(@PathVariable id: Long) =
