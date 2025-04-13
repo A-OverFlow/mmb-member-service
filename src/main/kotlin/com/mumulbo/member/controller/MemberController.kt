653302110e9af3954cd255fde1a9d9ca9a3f2df3
Joon Hee Song <mike.urssu@gmail.com>
2025-04-13 20:51:05 +0900

    Feat: 회원 정보 수정 api 구현 (#37)
    Resolves: #31
    Related to: #27

--- src/main/kotlin/com/mumulbo/member/controller/MemberController.kt
+++ src/main/kotlin/com/mumulbo/member/controller/MemberController.kt
@@ -1,8 +1,10 @@
 package com.mumulbo.member.controller
 
 import com.mumulbo.member.dto.request.MemberCreateRequest
+import com.mumulbo.member.dto.request.MemberUpdateRequest
 import com.mumulbo.member.dto.response.MemberCreateResponse
 import com.mumulbo.member.dto.response.MemberGetResponse
+import com.mumulbo.member.dto.response.MemberUpdateResponse
 import com.mumulbo.member.service.MemberService
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.ResponseStatus
@@ -29,6 +32,12 @@ class MemberController(
     @GetMapping("/{id}")
     fun getMember(@PathVariable id: Long): ResponseEntity<MemberGetResponse> {
         val response = memberService.getMember(id)
+        return ResponseEntity.ok(response)
+    }
+
+    @PutMapping("/{id}")
+    fun updateMember(@PathVariable id: Long, @RequestBody request: MemberUpdateRequest): ResponseEntity<MemberUpdateResponse> {
+        val response = memberService.updateMember(id, request)
         return ResponseEntity.ok(response)
     }
 
