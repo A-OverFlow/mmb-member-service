ee741fde2a8991d0d168d33590e6a06bba3462f3
devyy <devyyj@gmail.com>
2025-05-02 00:05:00 +0900

    통합 테스트 중

--- src/main/kotlin/com/mumulbo/member/controller/MemberController.kt
+++ src/main/kotlin/com/mumulbo/member/controller/MemberController.kt
@@ -1,31 +1,28 @@
 package com.mumulbo.member.controller
 
-import com.mumulbo.main
 import com.mumulbo.member.dto.MemberDto
 import com.mumulbo.member.dto.request.MemberCreateRequest
 import com.mumulbo.member.dto.request.MemberUpdateRequest
 import com.mumulbo.member.dto.response.MemberCheckResponse
-import com.mumulbo.member.dto.response.MemberCreateResponse
-import com.mumulbo.member.dto.response.MemberGetResponse
 import com.mumulbo.member.dto.response.MemberUpdateResponse
 import com.mumulbo.member.service.MemberService
-import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.DeleteMapping
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.PutMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestParam
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/api/v1/members")
 class MemberController(
     private val memberService: MemberService
 ) {
+    @GetMapping("/me")
+    fun getMyInfo(@RequestHeader("X-User-Id") userId: String): ResponseEntity<MemberDto> {
+        // todo 일단은 x-user-id가 이메일이지만 db에 저장된 user id로 변경해야함
+        //  user id로 검색하는 함수가 필요함
+        println("@GetMapping(\"/me\")")
+        println(userId)
+        return ResponseEntity.ok(memberService.getMember(userId))
+    }
+
     @PostMapping
     fun createMember(@RequestBody request: MemberCreateRequest): ResponseEntity<MemberDto> {
         val response = memberService.createMember(request)
@@ -50,7 +47,10 @@ class MemberController(
     }
 
     @PutMapping("/{id}")
-    fun updateMember(@PathVariable id: Long, @RequestBody request: MemberUpdateRequest): ResponseEntity<MemberUpdateResponse> {
+    fun updateMember(
+        @PathVariable id: Long,
+        @RequestBody request: MemberUpdateRequest
+    ): ResponseEntity<MemberUpdateResponse> {
         val response = memberService.updateMember(id, request)
         return ResponseEntity.ok(response)
     }
