93a43ef2d72036fcba41dc7a183a2c4ab6690b19
joonhee.song <mike.urssu@gmail.com>
2025-06-06 17:52:53 +0900

    Feat: Api 요청/응답 로깅 설정
    Resolves: #65

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,6 +28,9 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-validation")
 
+    implementation("io.github.oshai:kotlin-logging-jvm:7.0.7")
+    implementation("org.slf4j:slf4j-api:2.0.17")
+
     implementation("io.jsonwebtoken:jjwt-api:0.11.5")
     runtimeOnly("io.jsonwebtoken:jjwt-impl:0.11.5")
     runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.11.5")
